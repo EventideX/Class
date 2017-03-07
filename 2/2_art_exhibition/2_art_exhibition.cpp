2a133158245492c3e3ea521f01e6201cdc3a9902
EventideX <844058637@qq.com>
2017-03-07 18:27:41 +0800

    first

---
+++ 2/2_art_exhibition/2_art_exhibition.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	int i, j, k, n, m, s, t, a[1001] = { 0 }, b[1001];
+	cin >> n;
+	for (i = 0; i < n; i++)
+	{
+		cin >> j;
+		a[j]++;
+	}
+	s = 0;
+	t = 1;
+	while (t == 1)
+	{
+		t = 0;
+		for (i = 0; i < 1001; i++)
+		{
+			if (a[i] >= 1)
+			{
+				a[i]--;
+				s++;
+				t = 1;
+			}
+		}
+		if (t == 1) s--;
+	}
+	cout << s;
+	return 0;
+}
