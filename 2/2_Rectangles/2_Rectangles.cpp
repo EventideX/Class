aeaca2cac5cb9975ea82dc357c69f2644df46af9
EventideX <844058637@qq.com>
2017-03-07 18:37:22 +0800

    first

---
+++ 2/2_Rectangles/2_Rectangles.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i, j, k, n, m, s, t, a[101][101] = { 0 }, x, y, z;
+	cin >> n;
+	s = 0;
+	for (i = 0; i < n; i++)
+	{
+		cin >> x >> y >> z;
+		for (j = x; j < y; j++)
+			for (k = 0; k < z; k++)
+			{
+				a[j][k]++;
+				if (a[j][k] == 1) s++;
+			}
+	}
+	cout << s;
+	return 0;
+}
