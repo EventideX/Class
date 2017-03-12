3d2f58999c0db3923ffc84a98cce1729926f92fa
EventideX <844058637@qq.com>
2017-03-12 19:46:40 +0800

    second

---
+++ 3/3_Encrypted_note/3_Encrypted_note.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<cstring>
+#include<cstdio>
+using namespace std;
+
+int main()
+{
+	int i, j, k, n, m, s, t;
+	char a[100001];
+	fgets(a,100001,stdin);
+	i = 0;
+	n = strlen(a);
+	while (i < n)
+	{
+		if (a[i] == 'W')
+		{
+			if ((a[i + 1] == 'U') && (a[i + 2] == 'B'))
+			{
+				i += 3;
+				continue;
+			}
+		}
+		j = i + 1;
+		t = 0;
+		while (j < n - 2)
+		{
+			if (a[j] == 'W')
+			{
+				if ((a[j + 1] == 'U') && (a[j + 2] == 'B'))
+				{
+					t = 1;
+					break;
+				}	
+			}
+			j++;
+		}
+		for (k = i; k < j; k++) cout << a[k];
+		if (t == 1) cout << ' ';
+		i = j;
+	}
+	return 0;
+}
